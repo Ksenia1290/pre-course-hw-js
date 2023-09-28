961943b90e878c3956bd2509e5e2419865776fc8
Ksenia1290 <Ksu.tender@yandex.ru>
2023-09-28 22:09:11 +0300

    les

--- lesson-01/04.js
+++ lesson-01/04.js
@@ -1,13 +1,10 @@
 let x = 5;
 x += 3;
-x = x+y;
-console.log (x);
 x *= 2;
-x = x*y;
-console.log (x);
 x -= 4;
-x = x-y;
-console.log (x);
 x /= 3;
-x = x/y;
+x = x+3;
+x = x*2;
+x = x-4;
+x = x/3;
 console.log (x)
