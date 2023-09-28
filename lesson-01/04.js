d7334f1bd07c3c3ae031a662812b1562b737018e
Ksenia1290 <Ksu.tender@yandex.ru>
2023-09-29 00:33:22 +0300

    l

--- lesson-01/04.js
+++ lesson-01/04.js
@@ -1,6 +1,9 @@
 let x = 5;
 x += 3;
+console.log(x);
 x *= 2;
+console.log(x);
 x -= 4;
+console.log(x);
 x /= 3;
-console.log();
+console.log(x);
--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,4 +1,4 @@
 let str = "zzzzyzz"; 
 let substring="y";
- let a = (substring===0) ? ' нашел' : 'заблудился';
-console.log(str.includes(substring,0));    
+ let a = (substring||0) ? ' нашел' : 'заблудился';
+console.log(str.includes(substring));    
