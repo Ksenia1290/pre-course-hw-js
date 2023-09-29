0b922f618e7b69ca16a6058be28b53615d8a720b
Ksenia1290 <Ksu.tender@yandex.ru>
2023-09-29 17:32:56 +0300

    l

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,6 +1,8 @@
 let number = 1;
-
-console.log(number--); 
-console.log(--number); 
-console.log(number++); 
+console.log(number--);
+number = 2;
 console.log(++number);
+number = 4;
+console.log(--number);
+number = 1;
+console.log(number++);
