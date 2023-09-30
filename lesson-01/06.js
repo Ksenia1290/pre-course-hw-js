17a1f2d0f3dc76463ae539d02bf5f89712666f13
Ksenia1290 <Ksu.tender@yandex.ru>
2023-09-30 22:07:56 +0300

    1

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,4 +1,3 @@
 let str = "zzzzyzz"; 
-let substring="y";
- let a = (substring||0) ? ' нашел' : 'заблудился';
-console.log(str.includes(substring));    
+ let a = (str.indexOf('y')) ? ' нашел' : 'заблудился';
+console.log(a);    
